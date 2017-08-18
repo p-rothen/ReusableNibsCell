dcc77a21405078cacd2415a6435c454936f7cbd1
Pedro Rothen <p.rothen@outlook.cl>
2017-08-18 16:22:29 -0300

    Progress (?)

--- ReusableNibsCell/BaseTableViewCell.swift
+++ ReusableNibsCell/BaseTableViewCell.swift
@@ -9,11 +9,29 @@
 import UIKit
 
 class BaseTableViewCell: UITableViewCell {
-
+    @IBOutlet weak var labelxd: UILabel!
+   
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
         self.backgroundColor = UIColor.green
+    }
+    
+    override func awakeAfter(using aDecoder: NSCoder) -> Any? {
+        return self
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        Bundle.main.loadNibNamed("BaseTableViewCell", owner: self, options: nil)
+        self.backgroundColor = .green
+        self.addSubview(self.contentView)
+        self.labelxd.text = "xdxd"
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
--- ReusableNibsCell/ViewController.swift
+++ ReusableNibsCell/ViewController.swift
@@ -15,15 +15,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //self.tableView.register(BaseTableViewCell.self, forCellReuseIdentifier: self.id)
-        let nib = UINib(nibName: "BaseTableViewCell", bundle: nil)
-        self.tableView.register(nib, forCellReuseIdentifier: self.id)
+        self.tableView.register(ChildTableViewCell.self, forCellReuseIdentifier: self.id)
+//        let nib = UINib(nibName: "BaseTableViewCell", bundle: nil)
+//        self.tableView.register(nib, forCellReuseIdentifier: self.id)
+        self.tableView.estimatedRowHeight = 100
+        self.tableView.rowHeight = UITableViewAutomaticDimension
     }
 }
 
 extension ViewController : UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
